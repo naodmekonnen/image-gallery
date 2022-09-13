a68aaf8d21548eadebc1f7c44a4ef00e839f3b5d
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-09-13 19:10:03 +0000

    update js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,17 @@ const overlay = document.querySelector('overlay');
 
 /* Declaring the array of image filenames */
 
+let imgArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
+
 /* Declaring the alternative text for each image file */
+
+let imgalts = {
+    'img1':''
+    'img2':''
+    'img2':''
+    'img4':''
+    'img5':''
+}
 
 /* Looping through images */
 
